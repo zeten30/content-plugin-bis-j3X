9f51b9cc2a950f36ba06fc74e9b9b18160cb7437
Milan Zink <zeten30@gmail.com>
2015-01-08 14:47:20 +0100

    2015-01-08 #2

--- make_pkg.sh
+++ make_pkg.sh
@@ -3,9 +3,6 @@
 find . -name '*.php' -exec dos2unix {} \;
 find . -name '*.xml' -exec dos2unix {} \;
 
-if [ ! -d ./dist ]; then mkdir dist; fi;
-
 version=$(grep '<version>' bis.xml | sed -e 's/<[a-z\/]*>//g' | sed -e 's/ *//g')
-rm dist/plugin_bis*.zip
+rm plugin_bis*.zip
 zip -r plugin_bis-${version}-j3X.zip myr images cs-CZ.* en-GB.* bis.php bis.xml index.html
-mv plugin_bis-${version}*.zip dist/
